5ee2182a6b5172970acf2050d1faa82a7139ce8b
Ahsan Ali Shah <ahsanali.bscssef20@iba-suk.edu.pk>
2024-04-03 15:59:44 +0500

    added guessing game

--- app/src/main/java/com/example/mad_lab_1/MainActivity.kt
+++ app/src/main/java/com/example/mad_lab_1/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mad_lab_1
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -23,28 +24,40 @@ import androidx.compose.ui.unit.sp
 import com.example.mad_lab_1.ui.theme.MADlab1Theme
 import androidx.compose.ui.text.TextStyle
 
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MADlab1Theme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ShowButtons()
+                    ShowButtons( { task ->
+                        when (task) {
+                            1 -> navigateToGuessingGame()
+                            2 -> navigateToMrPotato()
+                        }
+                    })
                 }
             }
         }
     }
+    private fun navigateToGuessingGame() {
+        val intent = Intent(this, GuessingGameActivity::class.java)
+        startActivity(intent)
+    }
+    private fun navigateToMrPotato() {
+        val intent = Intent(this, MrPotatoActivity::class.java)
+        startActivity(intent)
+    }
 }
 
 @Composable
-fun ShowButtons(modifier: Modifier = Modifier) {
+fun ShowButtons(onTaskSelected: (Int) -> Unit,modifier: Modifier = Modifier) {
     Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
         Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.spacedBy(8.dp)) {
-            
             Text(
                 text = "MAD Lab 1",
                 style = TextStyle(
@@ -52,16 +65,14 @@ fun ShowButtons(modifier: Modifier = Modifier) {
                     fontWeight = FontWeight.Bold
                 )
             )
-
             Spacer(modifier = Modifier.height(32.dp)) // Add more space between the heading and buttons
 
-
-            Button(onClick = { /*TODO*/ }) {
+            Button(onClick = { onTaskSelected(1) }) {
                 Text(
                     text = "Guessing Game"
                 )
             }
-            Button(onClick = { /*TODO*/ }) {
+            Button(onClick = { onTaskSelected(2) }) {
                 Text(
                     text = "Mr. Potato"
                 )
@@ -70,10 +81,11 @@ fun ShowButtons(modifier: Modifier = Modifier) {
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MADlab1Theme {
-        ShowButtons()
-    }
-}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    MADlab1Theme {
+//        ShowButtons()
+//    }
+//}
---
+++ app/src/main/java/com/example/mad_lab_1/MrPotatoActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mad_lab_1
+
+class MrPotatoActivity {
+
+}
