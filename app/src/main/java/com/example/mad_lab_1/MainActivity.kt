3aee6f2d883615ff857adaf3b25b31454892cf1c
Ahsan Ali Shah <ahsanali.bscssef20@iba-suk.edu.pk>
2024-04-04 18:36:48 +0500

    setup frontpage

--- app/src/main/java/com/example/mad_lab_1/MainActivity.kt
+++ app/src/main/java/com/example/mad_lab_1/MainActivity.kt
@@ -3,15 +3,22 @@ package com.example.mad_lab_1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.mad_lab_1.ui.theme.MADlab1Theme
 
 
@@ -20,32 +27,34 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MADlab1Theme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    ShowButtons()
-                }
+                ShowButtons()
             }
         }
     }
 }
 
 @Composable
-fun ShowButtons(modifier: Modifier = Modifier) {
-    Box(modifier = Modifier.fillMaxSize()){
-        Column {
-            Text(text = "MAD Lab 1")
-            Button(onClick = { /*TODO*/ }) {
-                Text("Guessing Game")
-            }
-            Button(onClick = { /*TODO*/ }) {
-             Text(text = "Mr Potato")   
-            }
+fun ShowButtons() {
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "MAD Lab 1",
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        Spacer(modifier = Modifier.size(50.dp))
+
+        Button(onClick = { /*TODO*/ }) {
+            Text("Guessing Game")
         }
-        
-//        Text("Hello ")
-//        Text("World")
+        Button(onClick = { /*TODO*/ }) {
+         Text(text = "Mr Potato")
+        }
     }
 }
 
