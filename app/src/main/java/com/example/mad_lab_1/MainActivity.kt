8ad071c2a7df00c62ee780e9f83eda44de875124
Ahsan Ali Shah <ahsanali.bscssef20@iba-suk.edu.pk>
2024-04-04 18:00:11 +0500

    started from scratch

--- app/src/main/java/com/example/mad_lab_1/MainActivity.kt
+++ app/src/main/java/com/example/mad_lab_1/MainActivity.kt
@@ -1,28 +1,18 @@
 package com.example.mad_lab_1
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.mad_lab_1.ui.theme.MADlab1Theme
-import androidx.compose.ui.text.TextStyle
 
 
 class MainActivity : ComponentActivity() {
@@ -34,54 +24,31 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ShowButtons( { task ->
-                        when (task) {
-                            1 -> navigateToGuessingGame()
-                            2 -> navigateToMrPotato()
-                        }
-                    })
+                    ShowButtons()
                 }
             }
         }
     }
-    private fun navigateToGuessingGame() {
-        val intent = Intent(this, GuessingGameActivity::class.java)
-        startActivity(intent)
-    }
-    private fun navigateToMrPotato() {
-        val intent = Intent(this, MrPotatoActivity::class.java)
-        startActivity(intent)
-    }
 }
 
 @Composable
-fun ShowButtons(onTaskSelected: (Int) -> Unit,modifier: Modifier = Modifier) {
-    Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.spacedBy(8.dp)) {
-            Text(
-                text = "MAD Lab 1",
-                style = TextStyle(
-                    fontSize = 24.sp,
-                    fontWeight = FontWeight.Bold
-                )
-            )
-            Spacer(modifier = Modifier.height(32.dp)) // Add more space between the heading and buttons
-
-            Button(onClick = { onTaskSelected(1) }) {
-                Text(
-                    text = "Guessing Game"
-                )
+fun ShowButtons(modifier: Modifier = Modifier) {
+    Box(modifier = Modifier.fillMaxSize()){
+        Column {
+            Text(text = "MAD Lab 1")
+            Button(onClick = { /*TODO*/ }) {
+                Text("Guessing Game")
             }
-            Button(onClick = { onTaskSelected(2) }) {
-                Text(
-                    text = "Mr. Potato"
-                )
+            Button(onClick = { /*TODO*/ }) {
+             Text(text = "Mr Potato")   
             }
         }
+        
+//        Text("Hello ")
+//        Text("World")
     }
 }
 
-//
 //@Preview(showBackground = true)
 //@Composable
 //fun GreetingPreview() {
--- app/src/main/java/com/example/mad_lab_1/MrPotatoActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.mad_lab_1
-
-class MrPotatoActivity {
-
-}
