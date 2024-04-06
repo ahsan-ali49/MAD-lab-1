34cff1c763fb686799da116aa91c4cbfa167d4f6
Ahsan Ali Shah <ahsanali.bscssef20@iba-suk.edu.pk>
2024-04-06 20:43:37 +0500

    added mrpotato images

--- app/src/main/java/com/example/mad_lab_1/MrPotato.kt
+++ app/src/main/java/com/example/mad_lab_1/MrPotato.kt
@@ -3,8 +3,16 @@ package com.example.mad_lab_1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import com.example.mad_lab_1.ui.theme.MADlab1Theme
 
 class MrPotato : ComponentActivity() {
@@ -20,5 +28,22 @@ class MrPotato : ComponentActivity() {
 
 @Composable
 fun MrPotatoScreen() {
-    Text(text = "Mr Potato Screen")
+    Box (
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+//        verticalArrangement = Arrangement.Center,
+//        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Image(painter = painterResource(id = R.drawable.body), contentDescription = "body image")
+        Image(painter = painterResource(id = R.drawable.eyes), contentDescription = "eyes image")
+        Image(painter = painterResource(id = R.drawable.arms), contentDescription = "arms image")
+        Image(painter = painterResource(id = R.drawable.ears), contentDescription = "ears image")
+        Image(painter = painterResource(id = R.drawable.nose), contentDescription = "nose image")
+        Image(painter = painterResource(id = R.drawable.hat), contentDescription = "hat image")
+        Image(painter = painterResource(id = R.drawable.mouth), contentDescription = "mouth image")
+        Image(painter = painterResource(id = R.drawable.shoes), contentDescription = "shoes image")
+        Image(painter = painterResource(id = R.drawable.glasses), contentDescription = "glasses image")
+        Image(painter = painterResource(id = R.drawable.eyebrows), contentDescription = "eyebrows image")
+        Image(painter = painterResource(id = R.drawable.mustache), contentDescription = "mustache image")
+    }
 }
